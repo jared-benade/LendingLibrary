5112c9a7ff783a44fb485463661ddf04c25e8f99
Jared Benade <jared.benade@chillisoft.co.za>
2016-01-15 16:03:48 +0200

    Added initial tests to get project into jenkins

---
+++ Source/LendingLibrary.DB.Tests/Repositories/TestFake.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace LendingLibrary.DB.Tests.Repositories
+{
+    [TestFixture]
+    public class TestFake
+    {
+        [Test]
+        public void FakeTest()
+        {
+            //---------------Set up test pack-------------------
+            
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            Assert.AreEqual(1,1);
+            //---------------Test Result -----------------------
+
+        }
+
+    }
+}
---
+++ Source/LendingLibrary.Web.Tests/Controllers/TestHomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using LendingLibrary.Web.Controllers;
+using NUnit.Framework;
+
+namespace LendingLibrary.Web.Tests.Controllers
+{
+    [TestFixture]
+    public class TestHomeController
+    {
+        [Test]
+        public void Index_ShouldReturnView()
+        {
+            //---------------Set up test pack-------------------
+            var controller = CreateController();
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            var result = controller.Index() as ViewResult;
+            //---------------Test Result -----------------------
+            Assert.IsNotNull(result);
+        }
+
+        private static HomeController CreateController()
+        {
+            return new HomeController();
+        }
+    }
+}
